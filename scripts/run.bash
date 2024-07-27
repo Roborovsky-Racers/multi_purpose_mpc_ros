a819c36c4e9247a61b67a36c4e2de843fb8fc86b
Takashi Sato <t.sato17123@gmail.com>
2024-07-27 20:24:02 +0900

    rename to multi_purpose_mpc_ros

--- scripts/run.bash
+++ scripts/run.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-source $(ros2 pkg prefix multi_purpose_mpc)/.venv/bin/activate
-python3 $(ros2 pkg prefix multi_purpose_mpc)/lib/multi_purpose_mpc/simulation
+source $(ros2 pkg prefix multi_purpose_mpc_ros)/.venv/bin/activate
+python3 $(ros2 pkg prefix multi_purpose_mpc_ros)/lib/multi_purpose_mpc_ros/simulation
