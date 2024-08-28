169ee541a76c8b67f2127fc8850ba6a6dcbdafa2
Kenta Kato <kenta.kato.4321@gmail.com>
2024-08-28 23:49:21 +0900

    条件式をわかりやすい方に反転

--- multi_purpose_mpc_ros/multi_purpose_mpc_ros/mpc_controller.py
+++ multi_purpose_mpc_ros/multi_purpose_mpc_ros/mpc_controller.py
@@ -368,17 +368,17 @@ class MPCController(Node):
         self._wait_until_message_received(lambda: self._enable_control, 'control mode request', timeout)
 
     def _wait_until_trajectory_received(self, timeout: float = 30.) -> None:
-        if not self._cfg.reference_path.update_by_topic:
-            return
-        self._wait_until_message_received(lambda: self._trajectory, 'trajectory', timeout)
+        if self._cfg.reference_path.update_by_topic:
+            self._wait_until_message_received(lambda: self._trajectory, 'trajectory', timeout)
+        return
 
     def _wait_until_aw_sim_status_received(self, timeout: float = 30.) -> None:
         self._wait_until_message_received(lambda: self._current_laps, 'AWSIM status', timeout)
 
     def _wait_until_path_constraints_received(self, timeout: float = 30.) -> None:
-        if not self._cfg.reference_path.use_path_constraints_topic: # type: ignore
-            return
-        self._wait_until_message_received(lambda: self._reference_path.path_constraints, 'path constraints', timeout)
+        if self._cfg.reference_path.use_path_constraints_topic: # type: ignore
+            self._wait_until_message_received(lambda: self._reference_path.path_constraints, 'path constraints', timeout)
+        return
 
     def _publish_mpc_pred_marker(self, x_pred, y_pred):
         pred_marker_array = MarkerArray()
