54fd827ce7fc9a4356fcdc954ddee65fc70f856e
Takashi Sato <t.sato17123@gmail.com>
2024-08-29 00:21:19 +0900

    Update mpc_controller.py
    remove unneeded return

--- multi_purpose_mpc_ros/multi_purpose_mpc_ros/mpc_controller.py
+++ multi_purpose_mpc_ros/multi_purpose_mpc_ros/mpc_controller.py
@@ -370,7 +370,6 @@ class MPCController(Node):
     def _wait_until_trajectory_received(self, timeout: float = 30.) -> None:
         if self._cfg.reference_path.update_by_topic:
             self._wait_until_message_received(lambda: self._trajectory, 'trajectory', timeout)
-        return
 
     def _wait_until_aw_sim_status_received(self, timeout: float = 30.) -> None:
         self._wait_until_message_received(lambda: self._current_laps, 'AWSIM status', timeout)
