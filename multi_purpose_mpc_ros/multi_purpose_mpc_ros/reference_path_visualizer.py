3fcda51e3429494f9cfbcfd24aaaa4513d3d4691
Kenta Kato <kenta.kato.4321@gmail.com>
2024-10-24 01:05:46 +0900

    ref_path の segment_length の累積和を text marker で描画

--- multi_purpose_mpc_ros/multi_purpose_mpc_ros/reference_path_visualizer.py
+++ multi_purpose_mpc_ros/multi_purpose_mpc_ros/reference_path_visualizer.py
@@ -3,6 +3,7 @@
 import yaml
 from typing import NamedTuple
 import copy
+import numpy as np
 
 # ROS 2
 import rclpy
@@ -10,8 +11,9 @@ from rclpy.node import Node
 from ament_index_python.packages import get_package_share_directory
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, Vector3
 from visualization_msgs.msg import Marker, MarkerArray
+from std_msgs.msg import ColorRGBA
 
 # Multi_Purpose_MPC
 from multi_purpose_mpc_ros.core.map import Map
@@ -24,7 +26,8 @@ from multi_purpose_mpc_ros.common import convert_to_namedtuple, file_exists
 
 class ReferencePathVisualizer(Node):
     PKG_PATH: str = get_package_share_directory('multi_purpose_mpc_ros') + "/"
-    ADD_WP_MARKER: bool = False
+    WP_SPHERE_ENABLED: bool = True
+    LENGTH_TEXT_ENABLED: bool = True
 
     def __init__(self, config_path: str) -> None:
         super().__init__("reference_path_visualizer")
@@ -90,50 +93,89 @@ class ReferencePathVisualizer(Node):
         self._publish_ref_path_marker(self._reference_path)
 
     def _publish_ref_path_marker(self, ref_path: ReferencePath):
+
         ref_path_marker_array = MarkerArray()
-        m_base = Marker()
-        m_base.header.frame_id = "map"
-        m_base.ns = "ref_path"
-        m_base.type = Marker.LINE_STRIP
-        m_base.action = Marker.ADD
-        m_base.pose.position.z = 0.0
-        m_base.scale.x = 0.2
-        m_base.color.a = 0.7
-        m_base.color.r = 0.0
-        m_base.color.g = 0.0
-        m_base.color.b = 1.0
-        spheres = Marker()
-        spheres.header.frame_id = "map"
-        spheres.ns = "ref_path_point"
-        spheres.type = Marker.SPHERE_LIST
-        spheres.action = Marker.ADD
-        radius = 0.2
-        spheres.scale.x = radius
-        spheres.scale.y = radius
-        spheres.scale.z = radius
-        spheres.color.a = 0.7
-        spheres.color.r = 1.0
-        spheres.color.g = 1.0
-        spheres.color.b = 0.0
+
+        line_base = Marker()
+        line_base.header.frame_id = "map"
+        line_base.ns = "ref_path"
+        line_base.type = Marker.LINE_STRIP
+        line_base.action = Marker.ADD
+        line_base.pose.position.z = 0.0
+        line_base.scale.x = 0.2
+        line_base.color = ColorRGBA(r=0.0, g=0.0, b=1.0, a=0.7)
+
         for i in range(len(ref_path.waypoints) - 1):
-            m = copy.deepcopy(m_base)
-            m.id = i
+            line = copy.deepcopy(line_base)
+            line.id = i
             start = Point()
             start.x = ref_path.waypoints[i].x
             start.y = ref_path.waypoints[i].y
             end = Point()
             end.x = ref_path.waypoints[i + 1].x
             end.y = ref_path.waypoints[i + 1].y
-            m.points.append(start)
-            m.points.append(end)
-            ref_path_marker_array.markers.append(m)
-            if self.ADD_WP_MARKER:
+            line.points.append(start) # type: ignore
+            line.points.append(end) # type: ignore
+            ref_path_marker_array.markers.append(line) # type: ignore
+
+        if self.WP_SPHERE_ENABLED:
+            spheres = Marker()
+            spheres.header.frame_id = "map"
+            spheres.ns = "ref_path_point"
+            spheres.type = Marker.SPHERE_LIST
+            spheres.action = Marker.ADD
+            radius = 0.2
+            spheres.scale = Vector3(x=radius, y=radius, z=radius)
+            spheres.color = ColorRGBA(r=1.0, g=1.0, b=0.0, a=0.7)
+
+            length_cum = np.cumsum(self._reference_path.segment_lengths)
+
+            text_base = Marker()
+            text_base.header.frame_id = "map"
+            text_base.type = Marker.TEXT_VIEW_FACING
+            text_base.action = Marker.ADD
+            text_base.pose.position.z = 0.0
+            text_base.scale.z = 0.7
+            text_base.color = ColorRGBA(r=1.0, g=1.0, b=1.0, a=0.7)
+
+
+            for i in range(len(ref_path.waypoints) - 1):
                 p = Point()
                 p.x = ref_path.waypoints[i].x
                 p.y = ref_path.waypoints[i].y
-                p.z = 0.0
-                spheres.points.append(p)
-        ref_path_marker_array.markers.append(spheres)
+                p.z = 0.
+                spheres.points.append(p) #type: ignore
+
+                text_i = copy.deepcopy(text_base)
+                text_i.ns = f"ref_path_length_{i}"
+                text_i.pose.position = p
+                text_i.text = f"{i}_{length_cum[i]:.2f}"
+                ref_path_marker_array.markers.append(text_i) # type: ignore
+
+            ref_path_marker_array.markers.append(spheres) # type: ignore
+
+        if self.LENGTH_TEXT_ENABLED:
+            text_base = Marker()
+            text_base.header.frame_id = "map"
+            text_base.type = Marker.TEXT_VIEW_FACING
+            text_base.action = Marker.ADD
+            text_base.pose.position.z = 0.0
+            text_base.scale.z = 0.7
+            text_base.color = ColorRGBA(r=1.0, g=1.0, b=1.0, a=0.7)
+
+            length_cum = np.cumsum(self._reference_path.segment_lengths)
+            for i in range(len(ref_path.waypoints) - 1):
+                p = Point()
+                p.x = ref_path.waypoints[i].x
+                p.y = ref_path.waypoints[i].y
+                p.z = 0.
+
+                text_i = copy.deepcopy(text_base)
+                text_i.ns = f"ref_path_length_{i}"
+                text_i.pose.position = p
+                text_i.text = f"{i}_{length_cum[i]:.2f}"
+                ref_path_marker_array.markers.append(text_i) # type: ignore
+
         self._ref_path_pub.publish(ref_path_marker_array)
 
     @classmethod
